e044cd6c80caf141c80f24b45fcce6b42cf3a0af
AltRavenAR <tybziandparkeromg@gmail.com>
2023-11-18 22:14:26 -0500

    start

---
+++ dscbot.py
@@ -0,0 +1,32 @@
+import discord
+
+from discord.ext import commands
+client = commands.Bot
+intents = discord.Intents.all()
+client = commands.Bot(command_prefix="!", intents=intents)
+
+@client.event
+async def on_ready():
+	print('we have logged in as {0.user}'.format(client))
+
+
+@commands.command()
+async def ping(ctx):
+    """A response"""
+    await ctx.send("no bruv, don't even try.")
+
+@commands.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+
+@client.event
+async def on_message(message):
+	if message.author==client.user:
+		return
+	if message.content.startswith('?hello'):
+		await message.channel.send('Hello!')
+		
+
+client.run('MTE3NTYwMTM5MzQzNjk5MTUyOQ.Gyq9k7.iGfmIilyzrJcjnsOnZlBnCTiHBKterYtZ5FeFY')
