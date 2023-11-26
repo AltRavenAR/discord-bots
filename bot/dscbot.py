e6af324fa1ea45d85e623da2a82d27e25f2ecf00
AltRavenAR <tybziandparkeromg@gmail.com>
2023-11-26 01:29:34 -0500

    m

--- bot/dscbot.py
+++ bot/dscbot.py
@@ -14,25 +14,18 @@ async def on_ready():
     print(f'We have logged in as {client.user}')
 
 
-class MyCog(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
+@client.command
+async def ping(self, ctx):
+    """A response"""
+    await ctx.send("no bruv, don't even try.")
+	
 
-    @commands.command()
-    async def ping(self, ctx):
-        """A response"""
-        await ctx.send("no bruv, don't even try.")
-
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.author == self.bot.user:
-            return
-        if message.content.startswith('?hello'):
-            await message.channel.send('Hello!')
-
-
-client.add_cog(MyCog(client))
-
-		
+@client.command
+async def on_message(self, message):
+    if message.author == self.bot.user:
+        return
+    if message.content.startswith('?hello'):
+       await message.channel.send('Hello!')
+	   
 
 client.run(os.environ['TOKEN'])
